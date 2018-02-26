336ca84413fa18170e3f2283a1c8233ad23d8e69
chunky2808 <parasjain2896@gmail.com>
2018-02-27 00:18:12 +0530

    Second-Authentication Done

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse("hi There!")
+	return render(request,'home.html')
 
 
 def signup(request):
--- src/urls.py
+++ src/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^$', accounts_views.home, name='home'),
     
